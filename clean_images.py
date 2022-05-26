88fd7355cde074f36a2153036c9ed6c93c1e6660
Shahrukh Shah <scharukh.shah7@gmail.com>
2022-05-26 04:06:51 +0100

    working CNN model

--- clean_images.py
+++ clean_images.py
@@ -1,3 +1,4 @@
+#%%
 from PIL import Image
 import os
 from os import makedirs
@@ -5,14 +6,14 @@ import glob
 
 class CleanImagesFB():
     
-    def __init__(self, path: str='./images', target_folder: str='./new_images') -> None:
+    def __init__(self, path: str='data/images', target_folder: str='data/cleaned_images') -> None:
         self.path = path
         self.target_folder = target_folder
         self.images = glob.glob(self.path + '/*.jpg')
         self.resized = glob.glob(self.target_folder + '/*.jpg')
         makedirs(target_folder, exist_ok=True)
     
-    def clean_images(self, size: int=128) -> None:
+    def clean_images(self, size: int=64) -> None:
 
         final_size = (size, size)
         list_of_cleaned_images = [x.split("/")[-1]
@@ -20,7 +21,7 @@ class CleanImagesFB():
         for image in self.images:
             print("Image:")
             if str(image.split("/")[-1]) not in list_of_cleaned_images:
-                print(f'{image.split("/")[-1]} not in new_images')
+                print(f'{image.split("/")[-1]} not in cleaned_images')
                 print("Cleaning Image")
                 image_name = os.path.basename(image)
                 print(f'image_name: {image_name}')
@@ -40,8 +41,8 @@ class CleanImagesFB():
                 #paste on black background
                 black_img.paste(img, (int((final_size[0] - new_img_size[0]) // 2), int((final_size[1] - new_img_size[1]) // 2)))
                 #save Images
-                print(f'Saving cleaned image: new_images/{image_name}')
-                black_img.save(f'new_images/{image_name}')
+                print(f'Saving cleaned image: cleaned_images/{image_name}')
+                black_img.save(f'data/cleaned_images/{image_name}')
             else:
                 print(f'Image already cleaned: {image}')
 
@@ -49,3 +50,4 @@ if __name__ == '__main__':
     cleaner = CleanImagesFB()
     cleaner.clean_images()
     
+# %%
